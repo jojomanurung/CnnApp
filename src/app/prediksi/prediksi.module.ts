363080d4279bad4576088d1fe47d6f653535c9d6
Joshua Melvin <joshuamlvn@gmail.com>
2022-06-18 12:24:04 +0700

    Init component upload gambar

--- src/app/prediksi/prediksi.module.ts
+++ src/app/prediksi/prediksi.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PrediksiRoutingModule } from './prediksi-routing.module';
 import { PrediksiComponent } from './prediksi.component';
 import { SharedModule } from '../shared/shared.module';
+import { UploadGambarComponent } from './upload-gambar/upload-gambar.component';
 
 @NgModule({
-  declarations: [PrediksiComponent],
+  declarations: [PrediksiComponent, UploadGambarComponent],
   imports: [CommonModule, PrediksiRoutingModule, SharedModule],
 })
 export class PrediksiModule {}
---
+++ src/app/prediksi/upload-gambar/upload-gambar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-gambar',
+  templateUrl: './upload-gambar.component.html',
+  styleUrls: ['./upload-gambar.component.scss']
+})
+export class UploadGambarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
