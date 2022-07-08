f734172aeaa346c1924931b567b56161ce111f8e
Joshua Melvin <joshuamlvn@gmail.com>
2022-07-08 23:17:18 +0700

    Init hasil componen

--- src/app/prediksi/prediksi.module.ts
+++ src/app/prediksi/prediksi.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from '../shared/shared.module';
 import { UploadGambarComponent } from './upload-gambar/upload-gambar.component';
 import { TableDataComponent } from './table-data/table-data.component';
 import { PredictionComponent } from './prediction/prediction.component';
+import { HasilComponent } from './hasil/hasil.component';
 
 @NgModule({
-  declarations: [PrediksiComponent, UploadGambarComponent, TableDataComponent, PredictionComponent],
+  declarations: [PrediksiComponent, UploadGambarComponent, TableDataComponent, PredictionComponent, HasilComponent],
   imports: [CommonModule, PrediksiRoutingModule, SharedModule],
 })
 export class PrediksiModule {}
